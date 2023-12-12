9ead03f37d15adac1e5b5821ec09cc6c71bb0579
rinij <rinimani7@gmail.com>
2023-12-12 15:15:52 -0700

    header framework

--- grocery-App/src/App.tsx
+++ grocery-App/src/App.tsx
@@ -3,15 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import RegisterForm from "./components/RegisterForm";
 import LoginForm from "./components/LoginForm";
+
+
 import './App.css'
 
 function App() {
-  
+
   // const [count, setCount] = useState(0)
 
   return (
     <>
-    <div>Grocery App</div>
+   
+    
     <Routes>
       <Route path="/" element ={<Home />} />
       <Route path="/register" element={<RegisterForm />} />
--- grocery-App/src/pages/Home.tsx
+++ grocery-App/src/pages/Home.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Navbar from "../components/Navbar";
+import imagePath from "../assets/react.svg";
+import Footer from "../components/Footer";
 
 const Home = () => {
+  let items = ["Home","Your Shopping List", "Cart"];
   return (
-    <div>Home</div>
+    <>
+    <Navbar  brandName="My Grocery App" 
+     imageSrcPath={imagePath} 
+      navItems={items}/>
+    <div>This is home page</div>
+    <Footer/>
+    </>
   )
 }
 
