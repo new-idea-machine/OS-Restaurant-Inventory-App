24c6c407a00edde01b83a9f8146e85b17baed0b1
rinij <rinimani7@gmail.com>
2023-12-15 10:21:30 -0700

    Added the store item components to home page

--- grocery-App/src/pages/Home.tsx
+++ grocery-App/src/pages/Home.tsx
@@ -92,7 +92,23 @@
 
 // export default Home
 
+import { Col, Row } from "react-bootstrap";
+import { StoreItem } from "../components/StoreItem";
+import storeItems from "../data/items.json";
+
 
 export function Home() {
-    return <h1>Home</h1>
+    return (
+    <>
+ <h1>Welcome to Compare App!!</h1>
+
+    <Row md={2} xs={1} lg={3} xl={4} className="g-3">
+      {storeItems.map(item => (
+        <Col key={item.id}>
+          <StoreItem {...item} />
+        </Col>
+      ))}
+    </Row>
+  </>
+  )
   }
